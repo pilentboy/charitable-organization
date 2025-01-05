d4530945318190ed7a7a81d585efe1354f79e9e4
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-01-05 21:09:15 +0330

    add yekan font

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,22 @@
 const Register = () => {
-  return <h1> ثبت نام</h1>;
+  return (
+    <section className="h-full flex items-center justify-center">
+      <div className="w-full h-[500px] flex items-center justify-between border ">
+        {/* register form */}
+        <div className="w-1/2  h-full ">
+          <form>
+            <h1>عضویت</h1>
+          </form>
+        </div>
+
+        <img
+          src="https://shaaf-charity.ir/wp-content/plugins/arian-login//assets/images/login-pic.jpg"
+          alt="img"
+          className="w-1/2 h-full"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Register;
