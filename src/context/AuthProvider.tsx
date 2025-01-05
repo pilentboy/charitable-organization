362007eaad5bd98e98544947185d1951e3a260c8
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-01-05 15:06:50 +0330

    add loading state to AuthProvider

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -2,17 +2,24 @@ import { createContext, useContext, useState } from "react";
 
 const authContext = createContext<{
   loggedIn: boolean;
+  loading: boolean;
   setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }>({
   loggedIn: true,
+  loading: true,
   setLoggedIn: () => {},
+  setLoading: () => {},
 });
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [loggedIn, setLoggedIn] = useState<boolean>(true);
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   return (
-    <authContext.Provider value={{ loggedIn, setLoggedIn }}>
+    <authContext.Provider
+      value={{ loggedIn, setLoggedIn, loading, setLoading }}
+    >
       {children}
     </authContext.Provider>
   );
