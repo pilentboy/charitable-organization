ef1f742039150a7b31e39fd13c0d565faaeb1278
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-01-05 21:12:53 +0330

    add vazir font

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,11 @@
 const Register = () => {
   return (
     <section className="h-full flex items-center justify-center">
-      <div className="w-full h-[500px] flex items-center justify-between border ">
+      <div className="w-full h-[500px] flex items-center justify-between border rounded-lg overflow-hidden ">
         {/* register form */}
-        <div className="w-1/2  h-full ">
+        <div className="w-1/2  h-full py-8 px-6">
           <form>
-            <h1>عضویت</h1>
+            <h1 className="text-3xl font-bold">عضویت</h1>
           </form>
         </div>
 
