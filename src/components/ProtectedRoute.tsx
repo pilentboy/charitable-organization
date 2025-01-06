a6515397dada91f109981d3647105620427d280a
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-01-06 04:28:41 +0330

    change main title style in sm scressn

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from "react-router";
 const ProtectedRoute = () => {
   const { loggedIn } = useAuth();
 
-  return loggedIn ? <Outlet /> : <Navigate to="/authentication" replace />;
+  return loggedIn ? <Outlet /> : <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,11 @@ const Home = () => {
       {/* <header></header> */}
       <main className="w-full h-[500px]  rounded-lg">
         {/* title */}
-        <div className="flex flex-col items-center gap-2">
-          <h1 className="text-[#007F3D] font-[900]  text-[50px]">
+        <div className="flex flex-col items-center gap-3 sm:gap-6">
+          <h1 className="text-[#007F3D] font-[900]  text-3xl sm:text-[50px]">
             قربانی آنلاین
           </h1>
-          <div className="flex items-center text-[20px] flex-wrap  justify-center sm:text-[22px] font-[900] gap-[2px]">
+          <div className="flex items-center text-[20px] flex-wrap  justify-center sm:text-[22px] font-[900] gap-[3px]">
             <h2> پرداخت نذورات</h2>
             <span className="bg-[#ed1b35] rounded-tl-2xl rounded-br-2xl rounded-tr-sm rounded-bl-sm p-1 text-white">
               قربانی گوسفند
