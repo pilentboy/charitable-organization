dcd8595b82c3770395d9da065b2bcf0b3b131263
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-01-06 03:50:52 +0330

    refactor LoginOTP

--- src/pages/LoginOTP.tsx
+++ src/pages/LoginOTP.tsx
@@ -1,32 +1,33 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link } from "react-router";
+import { useState } from "react";
+
 
 const LoginOTP = () => {
-  const [OTP, setOTP] = useState("");
+  const { register, handleSubmit } = useForm();
   const [otpType, setOTPType] = useState<string>("password");
 
-  const handleOTPLogin = (e: React.FormEvent) => {
-    e.preventDefault();
+  const onSubmit = (data: any) => {
+    console.log(data);
   };
 
   return (
     <section className="h-screen sm:h-full flex justify-center items-center  ">
       <div className="w-full h-[400px]  flex items-center justify-center border rounded-2xl overflow-hidden sm:h-[460px]">
         <form
-          onSubmit={handleOTPLogin}
+          onSubmit={handleSubmit(onSubmit)}
           className="w-full  py-8 px-6 flex flex-col gap-8 mx-auto lg:w-1/2 sm:gap-5 "
         >
           <h1 className="text-3xl font-bold mb-2 text-center">ورود</h1>
           <label htmlFor="otp">لطفا رمز ارسال شده را وارد کنید</label>
           <div className="flex justify-between items-center border border-gray-300 bg-gray-100 rounded-2xl h-12 p-2 duration-200 focus:border-gray-800">
             <input
+              {...register("otp")}
               name="otp"
               autoFocus
               id="otp"
               type={otpType}
-              value={OTP}
-              onChange={(e) => setOTP(e.target.value)}
               className="w-[90%] h-full bg-transparent outline-none border-none"
             />
             <button
