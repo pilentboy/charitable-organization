8e5e7d606314ca99a995212253bac475d89a5dbe
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-01-10 01:54:10 +0330

    display nothing in slider if there's no data

--- src/components/Home/Slider.tsx
+++ src/components/Home/Slider.tsx
@@ -18,22 +18,22 @@ const Slider = () => {
     getData();
   }, []);
 
-  return (
-    <Swiper
-      modules={[Pagination, A11y, Autoplay]}
-      spaceBetween={50}
-      slidesPerView={1}
-      // pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      loop={true}
-      autoplay={{
-        delay: 5000,
-        disableOnInteraction: false,
-      }}
-      className="w-full  rounded-2xl h-[200px] sm:h-[300px] lg:h-[480px] "
-    >
-      {sliderContents?.length > 0 ? (
-        sliderContents.map((slider: any) => {
+  if (sliderContents?.length > 0) {
+    return (
+      <Swiper
+        modules={[Pagination, A11y, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        // pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        loop={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        className="w-full  rounded-2xl h-[200px] sm:h-[300px] lg:h-[480px] "
+      >
+        {sliderContents.map((slider: any) => {
           console.log(slider.media);
           return (
             <SwiperSlide key={slider.id}>
@@ -45,14 +45,10 @@ const Slider = () => {
               />
             </SwiperSlide>
           );
-        })
-      ) : (
-        <SwiperSlide className="flex items-center justify-center w-full h-full bg-gray-400">
-          <h1>درحال دریافت اطلاعات</h1>
-        </SwiperSlide>
-      )}
-    </Swiper>
-  );
+        })}
+      </Swiper>
+    );
+  }
 };
 
 export default Slider;
