1335497d03c47bc1b543f68278ff7c07c4491841
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-01-12 01:45:45 +0330

    add comments for Layout  pagee

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,12 @@
-import { Outlet } from "react-router";
-import Nav from "./Header/Nav";
+import { Outlet } from "react-router"; // Importing Outlet to render nested routes
+import Nav from "./Header/Nav"; // Importing the Nav component for the navigation bar
 
-const   Layout = () => (
-  <div className="container  min-h-dvh py-4 ">
-    <Nav />
-    <Outlet />
+// Layout component that wraps around page content and includes the navigation bar
+const Layout = () => (
+  <div className="container min-h-dvh py-4"> {/* A container with a minimum height and padding */}
+    <Nav /> {/* Rendering the Nav component */}
+    <Outlet /> {/* Rendering the nested routes/components here */}
   </div>
 );
 
-export default Layout;
+export default Layout; // Exporting the Layout component for use in routing
