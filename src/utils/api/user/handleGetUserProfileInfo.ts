b3acbaff6459670fdf3036f68a21f274483501c7
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-01-12 01:49:47 +0330

    add comments for handleGetUserProfileInfo  function

--- src/utils/api/user/handleGetUserProfileInfo.ts
+++ src/utils/api/user/handleGetUserProfileInfo.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+// Importing the axios library for making HTTP requests
 
 const handleGetUserProfileInfo = async (accessToken: string) => {
   try {
@@ -10,11 +11,18 @@ const handleGetUserProfileInfo = async (accessToken: string) => {
         },
       }
     );
+    // Making a GET request to the user profile info endpoint with authorization header
+
     console.log(res, "user profile info");
+    // Logging the response with a custom message
+
     return res;
+    // Returning the response
   } catch (error) {
     console.log(error);
+    // Logging any errors that occur during the request
   }
 };
 
 export default handleGetUserProfileInfo;
+// Exporting the handleGetUserProfileInfo function as the default export
