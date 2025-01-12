535efd11e13a3c13b56eaeab14be26165f8966d7
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-01-12 23:53:49 +0330

    add bold font to quotes content tag

--- src/components/Home/Quotes.tsx
+++ src/components/Home/Quotes.tsx
@@ -26,7 +26,7 @@ const Quotes = () => {
       <div className="flex flex-col gap-1 pb-4" key={content.id}>
         <section className="flex  w-full h-fit items-center justify-center">
           <div className="w-full   items-center lg:w-[65%] gap-1   flex flex-col ">
-            <p className="sm:w-5/6 text-justify sm:text-start ">
+            <p className="sm:w-5/6 text-justify sm:text-start font-bold ">
               {content.content}
             </p>
             <span className="text-gray-700   text-sm">{content.title}</span>
