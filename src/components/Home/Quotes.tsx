1d0bc6ca6761f2e05e0000778d0940948d7e9ccc
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-01-15 00:18:57 +0330

    center the items in the Quotes

--- src/components/Home/Quotes.tsx
+++ src/components/Home/Quotes.tsx
@@ -11,7 +11,7 @@ const Quotes = () => {
           "https://nazronline.ir/api/content/quotes/"
         );
         setQuitesContent(response.data);
-        console.log(response.data)
+        console.log(response.data);
       } catch (error: any) {
         console.log(error);
         return error.message || "مشکلی پیش آمد";
@@ -21,23 +21,26 @@ const Quotes = () => {
     handleGetQuotes();
   }, []);
 
-
-
   if (quitesContent)
     return quitesContent?.map((content: any) => (
-      <div className="flex flex-col gap-1 pb-4" key={content.id}>
-        <section className="flex flex-col gap-2  w-full h-fit items-center justify-center">
-          <div className="w-full   items-center lg:w-[65%] gap-1   flex flex-col ">
-            <pre className="sm:w-5/6 text-justify sm:text-start font-bold text-wrap">
-              {content.content}
-            </pre>
-            <span className="text-gray-700   text-sm">{content.title}</span>
-          </div>
-          {content.media && (
-            <img src={`https://nazronline.ir/${content.media}`} className="w-full sm:w-[800px] rounded-md" alt={content.title} />
-          )}
-        </section>
-      </div>
+      <section
+        key={content.id}
+        className="flex flex-col gap-2   w-full h-fit items-center justify-center"
+      >
+        <div className="w-full justify-center  items-center text-center lg:w-[65%] gap-1   flex flex-col ">
+          <pre className="text-center sm:text-start font-bold text-wrap">
+            {content.content}
+          </pre>
+          <span className="text-gray-700   text-sm">{content.title}</span>
+        </div>
+        {content.media && (
+          <img
+            src={`https://nazronline.ir/${content.media}`}
+            className="w-full sm:w-[800px] rounded-md"
+            alt={content.title}
+          />
+        )}
+      </section>
     ));
 };
 
